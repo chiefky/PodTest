f7101f6fcd9af9db90212d9a0c6d4a7d54314c55
bjkangying <bjkangying@corp.netease.com>
2016-09-19 11:21:11 +0800

    add demo

---
+++ PodTestdemo/PodTestdemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PodTestdemo
+//
+//  Created by ciefky on 16/9/19.
+//  Copyright © 2016年 ciefky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
